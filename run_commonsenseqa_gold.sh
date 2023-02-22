30524529e492949597b55cd3d35bf9fc482237aa
Raymond Li <li.raymond96@gmail.com>
2023-02-22 14:32:20 -0800

    experiments

--- run_commonsenseqa_gold.sh
+++ run_commonsenseqa_gold.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1      # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6      # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=64000M            # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=commonsense_qa_gold.out
 
 module load python
